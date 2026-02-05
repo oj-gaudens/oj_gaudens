f435aaf81405f65cbc8db918574f59007e3666f1
oumar-julien gaudens <oumar-julien.gaudens@i-carre.net>
2026-02-05 10:14:50 +0100

    tezst

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = function (eleventyConfig) {
-  // Copie des assets (CSS, JS, images)
+  // Copie TOUT le dossier assets
   eleventyConfig.addPassthroughCopy("src/assets");
-
+  
+  // OU copiez chaque type séparément (plus sûr)
+  eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/assets/js");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  
   return {
-    pathPrefix: isProd ? "/oj.gaudens/" : "/",
+    pathPrefix: isProd ? "/oj_gaudens/" : "/",
     dir: {
       input: "src",
       includes: "_includes",
