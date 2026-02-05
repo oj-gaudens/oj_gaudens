ac7c15dc328dc89f3ccfe1980850d0828045ec64
oumar-julien gaudens <oumar-julien.gaudens@i-carre.net>
2026-02-05 14:20:10 +0100

    test1.2

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = function (eleventyConfig) {
-  // Copie TOUT le dossier assets (CSS, JS, images)
   eleventyConfig.addPassthroughCopy("src/assets");
   
   return {
-    pathPrefix: isProd ? "/oj-gaudens/" : "/",
+    pathPrefix: isProd ? "/oj.gaudens/" : "/",  // ⬅️ POINT, pas tiret !
     dir: {
       input: "src",
       includes: "_includes",
